235e2153cac8233e30d9df15ccae8ff77258fe3d
Romain SERRETTE <futuriste2001@gmail.com>
2026-09-23 09:27:11 +0200

    add card

--- dbe-event/src/App.jsx
+++ dbe-event/src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Navbar2 from './folder/Navbar2.jsx'
 import Login from './login/Login.jsx'
-import Accueil from './Accueil/Accueil.jsx'
+import Acceuil from './Accueil/Accueil.jsx'
 import Event from './Card/Card.jsx'
-
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <> 
     <Navbar2 />
-    <Accueil />
+    <Acceuil />
     <Event />
     </>
   )
